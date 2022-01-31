03fbe2e5612eaa8e6f0804d5f228bfa9db419d97
clark <clarkkenticloud@icloud.com>
2022-01-31 14:05:41 +0800

    init

---
+++ src/main/java/com/clark/password_book/PasswordBookApplication.java
@@ -0,0 +1,13 @@
+package com.clark.password_book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasswordBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clark/password_book/PasswordBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clark.password_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
